58912d62635857ef194d119bf8018c333686b01d
Mustavataj13 <klea.mustavataj@cit.edu.al>
2024-12-22 14:20:28 +0100

    Add a comment

--- University-Rest/src/main/java/com/university/service/DepartmentService.java
+++ University-Rest/src/main/java/com/university/service/DepartmentService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DepartmentService {
-
+    // This class does something
     @Autowired
     DepartmentRepository departmentRepository;
 
